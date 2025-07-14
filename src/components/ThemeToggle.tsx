026f92e2300e98ee10ca8e54a7893b8fa0aad86d
rosyidrid <rosyidridho9g@gmail.com>
2025-07-14 21:08:22 +0700

    update

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from '../context/useTheme';
+import { useTheme } from '../context/ThemeContext';
 import { FiSun, FiMoon } from 'react-icons/fi';
 
 export default function ThemeToggle() {
